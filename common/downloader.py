c50e28d285c9b5e902583ebfd3b6d268249c9490
Gabriel Garcia <garciacaysahuana@gmail.com>
2024-11-10 11:33:17 -0500

    Sin cambios

--- common/downloader.py
+++ common/downloader.py
@@ -3,5 +3,3 @@ import yfinance as yf
 
 def downloader_data(tickers: list, date_start: str, date_end: str):
     return yf.download(tickers, start = date_start, end = date_end)["Adj Close"].dropna()
-
-
